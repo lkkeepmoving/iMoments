5b32abf9a4baad898468abb6cba3a54cd9c597cd
lkkeepmoving <lkkeepmoving@gmail.com>
2013-08-25 16:20:19 -0700

    Initial Commit

---
+++ iMoments/iMomentsAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  iMomentsAppDelegate.h
+//  iMoments
+//
+//  Created by Kai Lu on 13-8-25.
+//  Copyright (c) 2013年 Kai Lu and Ziyu Ou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class iMomentsViewController;
+
+@interface iMomentsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) iMomentsViewController *viewController;
+
+@end
